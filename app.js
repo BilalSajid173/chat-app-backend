081b383c0e9ed3bdafe30e57c8680dd4d2d6f8b9
Bilal <bsajid173@gmail.com>
2022-06-19 20:58:23 +0530

    Create login and signup controllers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
-const authRoutes = require("./routes/auth")
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/auth", authRoutes)
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   const message = error.message;
--- routes/auth.js
+++ routes/auth.js
@@ -2,29 +2,38 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator/check");
 const User = require("../models/user");
+const authControllers = require("../controllers/auth");
 
-router.post("/signup", [
-  body("email")
-    .isEmail()
-    .withMessage("Please Enter a valid email address")
-    .custom((value, { req }) => {
-      return User.findOne({ email: value }).then((userDoc) => {
-        if (userDoc) {
-          return Promise.reject("Email Already exists");
-        }
-      });
-    })
-    .normalizeEmail(),
-  body("password").trim().isLength({ min: 7 }),
-  body("name").trim().not().isEmpty(),
-]);
+router.post(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please Enter a valid email address")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("Email Already exists");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 7 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authControllers.signup
+);
 
-router.post("/login", [
-  body("email")
-    .isEmail()
-    .withMessage("Please Enter a valid email")
-    .normalizeEmail(),
-  body("password").trim().isLength({ min: 7 }),
-]);
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please Enter a valid email")
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 7 }),
+  ],
+  authControllers.login
+);
 
 module.exports = router;
